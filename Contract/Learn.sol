549325607a836e5296adcf03ec02652b264b10d2
karl14000 <81111824+karl14000@users.noreply.github.com>
2022-03-16 15:29:20 +0100

    readme

--- Contract/Learn.sol
+++ Contract/Learn.sol
@@ -10,9 +10,9 @@ contract ValueTypes {
      
      uint public u = 123;
      
-     address public addr = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+     address public addr = ;
      
-     bytes32 public b32 = 0x89c58ced8a9078bdef2bb60f22e58eeff7dbfed6c2dff3e7c508b629295926fa;
+     bytes32 public b32 = ;
 
 
   
